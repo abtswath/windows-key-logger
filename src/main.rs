9abb36ecd023ce35489daac4d228158f52f4258f
abtswath <zyapfine@gmail.com>
2023-04-14 17:18:05 +0800

    refactor: record with window title and time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod logger;
 
-use std::{mem, sync::RwLock};
+use std::mem;
 
 use log::{debug, error, info, Level};
 use tokio::{
@@ -8,14 +8,14 @@ use tokio::{
     sync::oneshot::{channel, Sender},
 };
 use windows::Win32::{
-    Foundation::{LPARAM, LRESULT, WPARAM},
+    Foundation::{HWND, LPARAM, LRESULT, WPARAM},
     System::LibraryLoader::GetModuleHandleA,
     UI::{
         Input::KeyboardAndMouse::GetKeyNameTextA,
         WindowsAndMessaging::{
-            CallNextHookEx, DispatchMessageA, GetMessageA, SetWindowsHookExA, TranslateMessage,
-            UnhookWindowsHookEx, HHOOK, KBDLLHOOKSTRUCT, MSG, WH_KEYBOARD_LL, WM_KEYDOWN, WM_KEYUP,
-            WM_SYSKEYDOWN, WM_SYSKEYUP,
+            CallNextHookEx, DispatchMessageA, GetForegroundWindow, GetMessageA, GetWindowTextA,
+            GetWindowTextLengthA, SetWindowsHookExA, TranslateMessage, UnhookWindowsHookEx, HHOOK,
+            KBDLLHOOKSTRUCT, MSG, WH_KEYBOARD_LL, WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, WM_SYSKEYUP,
         },
     },
 };
@@ -24,31 +24,55 @@ struct Container<'a>(&'a mut (dyn RecordWriter + 'a));
 
 static mut CONTAINER: *mut Container<'static> = 0 as *mut _;
 
-static KEYS: RwLock<Vec<Key>> = RwLock::new(vec![]);
-
-struct Key {
-    key: KBDLLHOOKSTRUCT,
-    released: bool,
-}
-
-impl Key {
-    fn new(key: KBDLLHOOKSTRUCT) -> Self {
-        Key {
-            key,
-            released: false,
-        }
-    }
-}
-
 #[derive(Debug)]
 struct Record {
-    keys: Vec<KBDLLHOOKSTRUCT>,
-    key_text: String,
+    key: KBDLLHOOKSTRUCT,
+    hwnd: HWND,
 }
 
 impl Record {
-    fn new(keys: Vec<KBDLLHOOKSTRUCT>, key_text: String) -> Self {
-        Record { keys, key_text }
+    fn new(key: KBDLLHOOKSTRUCT, hwnd: HWND) -> Self {
+        Record { key, hwnd }
+    }
+
+    fn time(&self) -> u32 {
+        self.key.time
+    }
+
+    fn window_title(&self) -> String {
+        let len: usize = unsafe { GetWindowTextLengthA(self.hwnd) } as usize;
+        debug!(">>>>>>>>>>>>>>>>>>>>>>>>>>> {}", len);
+        let mut lp_string = vec![0; len + 1];
+        debug!(">>>>>>>>>>>>>>>>>>>>>>>>>>> {}", lp_string.len());
+        if unsafe { GetWindowTextA(self.hwnd, &mut lp_string) } > 0 {
+            if let Ok(title) = String::from_utf8(lp_string) {
+                return title;
+            }
+        } else {
+            debug!("cannot get window text...");
+        }
+        String::from("unknown")
+    }
+
+    fn key_text(&self) -> String {
+        let mut lp_string: [u8; 16] = [0; 16];
+
+        let key_text;
+        if unsafe { GetKeyNameTextA((self.key.scanCode << 16) as i32, &mut lp_string) } <= 0 {
+            debug!("failed to get key text: {:#?}", self.key);
+            key_text = String::from("unknown").into();
+        } else {
+            match String::from_utf8(lp_string.to_vec()) {
+                Ok(s) => {
+                    key_text = s.clone().trim_end_matches('\0').to_string();
+                }
+                Err(e) => {
+                    error!("unable to get key text from [{:#?}]: {}", lp_string, e);
+                    key_text = String::from("unknown").into();
+                }
+            };
+        }
+        key_text
     }
 }
 
@@ -60,8 +84,12 @@ struct ConsoleWriter {}
 
 impl RecordWriter for ConsoleWriter {
     fn write(&mut self, record: Record) {
-        debug!("the record keys: {:#?}", record.keys);
-        info!("the key [{}] has been triggered", record.key_text);
+        info!(
+            "{} - the key [{}] has been triggered, the window title is \"{}\"",
+            record.time(),
+            record.key_text(),
+            record.window_title()
+        );
     }
 }
 
@@ -103,26 +131,6 @@ async fn install_keyboard_hook(sender: Sender<HHOOK>) {
     };
 }
 
-fn get_key_text(key: KBDLLHOOKSTRUCT) -> String {
-    let mut lp_string: [u8; 16] = [0; 16];
-    let key_text;
-    if unsafe { GetKeyNameTextA((key.scanCode << 16) as i32, &mut lp_string) } <= 0 {
-        debug!("failed to get key text:{:#?}", key);
-        key_text = String::from("unknown").into();
-    } else {
-        match String::from_utf8(lp_string.to_vec()) {
-            Ok(s) => {
-                key_text = s.clone().trim_end_matches('\0').to_string();
-            }
-            Err(e) => {
-                error!("unable to get key text from [{:#?}]: {}", lp_string, e);
-                key_text = String::from("unknown").into();
-            }
-        };
-    }
-    key_text
-}
-
 unsafe extern "system" fn low_level_keyboard_proc(
     n_code: i32,
     w_param: WPARAM,
@@ -137,47 +145,11 @@ unsafe extern "system" fn low_level_keyboard_proc(
                 "the key down event has been triggered: {:#?}",
                 (*key).vkCode
             );
-            match KEYS.write() {
-                Ok(mut keys) => {
-                    debug!("new key has been pressed...");
-                    (*keys).push(Key::new(*key));
-                }
-                Err(e) => {
-                    error!("cannot get keys (key_down): {}", e);
-                }
-            }
+            let c = unsafe { &mut *CONTAINER };
+            c.0.write(Record::new(*key, GetForegroundWindow()));
         }
         WM_SYSKEYUP | WM_KEYUP => {
             debug!("the key up event has been triggered: {:#?}", (*key).vkCode);
-            match KEYS.write() {
-                Ok(mut keys) => {
-                    debug!("new key has been released...");
-                    let mut i = (*keys).len();
-                    while i > 0 {
-                        i -= 1;
-                        if !(*keys)[i].released {
-                            (*keys)[i].released = true;
-                            break;
-                        }
-                    }
-                    if i == 0 {
-                        debug!("all keys has been released...");
-                        let mut record_text = vec![];
-                        let mut record_keys = vec![];
-                        keys.iter().for_each(|key| {
-                            record_keys.push(key.key);
-                            record_text.push(get_key_text(key.key));
-                        });
-                        (*keys).clear();
-
-                        let c = unsafe { &mut *CONTAINER };
-                        c.0.write(Record::new(record_keys, record_text.join(" + ")));
-                    }
-                }
-                Err(e) => {
-                    error!("cannot get keys (key_up): {}", e);
-                }
-            }
         }
         _ => {
             debug!(
